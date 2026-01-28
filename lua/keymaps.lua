61208953f2af5fdf9ebf3865bc0499aa6f2ff81b
Alexander Nyström <alexander.nystrom@oneflow.com>
2026-01-28 17:28:11 +0100

    remove autocreate dir

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,16 +26,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank { timeout = 200 } end,
 })
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-  desc = 'Auto-create missing dirs when saving a file',
-  group = vim.api.nvim_create_augroup('kickstart-auto-create-dir', { clear = true }),
-  pattern = '*',
-  callback = function()
-    local dir = vim.fn.expand '<afile>:p:h'
-    if vim.fn.isdirectory(dir) == 0 then
-      vim.fn.mkdir(dir, 'p')
-    end
-  end,
-})
-
 -- vim: ts=2 sts=2 sw=2 et
